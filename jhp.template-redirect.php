c2f2b2f899c31e754247a51ff3534a06edf71099
Joe Hoyle <joehoyle@gmail.com>
2010-03-19 11:42:21 +0000

    Fixed bug whereby Portfolio would appear multiple times on the Portfolio homepage when using widgets

--- jhp.template-redirect.php
+++ jhp.template-redirect.php
@@ -19,9 +19,15 @@ function jhp_template_redirect() {
 		$portfolio_single_files[] = get_template_directory() . '/' . $file;
 	$portfolio_single_files[] = dirname( __FILE__ ) . '/template/portfolio.single.php';
 	
-		
+	// only set up the main query if they are using custom templates
+	if( ($file = get_option( 'jhp_template_home', 'portfolio-home.php')) && file_exists( $file ) ) {
+		$portfolio_query = array( 'post_type' => 'jh-portfolio', 'paged' => 1 );
+	} else {
+		$portfolio_query = array( 'post_type' => 'jh-portfolio', 'showposts' => '1' );
+	}
+			
 	// add the rewrites
-	tj_add_page_rule( "^/$portfolio_base(/page/([0-9]*))?/?", $portfolio_files, 'Portfolio', null, false, array('is_page' => '1', 'is_portfolio' => '1'), array( 'post_type' => 'jh-portfolio', 'paged' => 1 ) );
+	tj_add_page_rule( "^/$portfolio_base(/page/([0-9]*))?/?", $portfolio_files, 'Portfolio', null, false, array('is_page' => '1', 'is_portfolio' => '1'), $portfolio_query );
 	tj_add_page_rule( "^/$portfolio_base" . ( $single_base ? "/$single_base" : "" ) . "/([^\/]*)/?", $portfolio_single_files, 'Portfolio Single', null, false, array( 'is_single' => '1', 'is_portfolio_single' => '1' ), array( 'name' => 0, 'post_type' => 'jh-portfolio' ) );
 	tj_add_page_rule( "^/$portfolio_base/([^/]*)/?", $portfolio_files, 'Portfolio', null, false, array('is_page' => '1', 'is_portfolio' => '1'), array( 'taxonomy' => 'jh-portfolio-category', 'term' => 0, 'post_type' => 'jh-portfolio' ) );
 }
