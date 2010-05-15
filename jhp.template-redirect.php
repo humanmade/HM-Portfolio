596b9a66539e601e8d1f0cc96ffbeb71bbe03420
Joe Hoyle <joehoyle@gmail.com>
2010-05-15 18:24:29 +0100

    Modified all rewrites to use standard rewrite api - fixes preview post / not having permalinks enabled / saves wasted queries on blog

--- jhp.template-redirect.php
+++ jhp.template-redirect.php
@@ -1,34 +1,19 @@
 <?php
-add_action('template_redirect', 'jhp_template_redirect', 2);
+$portfolio_base = get_option('jhp_url_base', 'portfolio');
 
-function jhp_template_redirect() {
-	global $wp_query;
-	
-	$portfolio_base = get_option('jhp_url_base', 'portfolio');
-		 
-	$single_base = untrailingslashit(get_option('jhp_single_base', '%category%'));
-	$single_base = trim( str_ireplace( '%category%', '[^\/]*', $single_base ), '/' );
-	
-	$portfolio_files = array();
-	if( $file = get_option( 'jhp_template_home', 'portfolio-home.php' ) )
-		$portfolio_files[] = get_template_directory() . '/' . $file;
-	$portfolio_files[] = dirname( __FILE__ ) . '/template/portfolio.php';
-	
-	$portfolio_single_files = array();
-	if( $file = get_option( 'jhp_template_single', 'portfolio-single.php' ) )
-		$portfolio_single_files[] = get_template_directory() . '/' . $file;
-	$portfolio_single_files[] = dirname( __FILE__ ) . '/template/portfolio.single.php';
-	
-	// only set up the main query if they are using custom templates
-	if( ($file = get_option( 'jhp_template_home', 'portfolio-home.php')) && file_exists( $file ) ) {
-		$portfolio_query = array( 'post_type' => 'jh-portfolio', 'paged' => 1 );
-	} else {
-		$portfolio_query = array( 'post_type' => 'jh-portfolio', 'showposts' => '1' );
-	}
-			
-	// add the rewrites
-	tj_add_page_rule( "^/$portfolio_base(/page/([0-9]*))?/?", $portfolio_files, 'Portfolio', null, false, array('is_page' => '1', 'is_portfolio' => '1'), $portfolio_query );
-	tj_add_page_rule( "^/$portfolio_base" . ( $single_base ? "/$single_base" : "" ) . "/([^\/]*)/?", $portfolio_single_files, 'Portfolio Single', null, false, array( 'is_single' => '1', 'is_page' => '1', 'is_portfolio_single' => '1' ), array( 'name' => 0, 'post_type' => 'jh-portfolio' ) );
-	tj_add_page_rule( "^/$portfolio_base/([^/]*)/?", $portfolio_files, 'Portfolio', null, false, array('is_page' => '1', 'is_portfolio' => '1'), array( 'taxonomy' => 'jh-portfolio-category', 'term' => 0, 'post_type' => 'jh-portfolio' ) );
-}
+//portfolio home
+if( file_exists( $portfolio_file = get_template_directory() . '/' . get_option( 'jhp_template_home', 'portfolio-home.php' ) ) );
+else $portfolio_file = dirname( __FILE__ ) . '/template/portfolio.php';
+tj_add_rewrite_rule( "^$portfolio_base(/page/([0-9]*))?/?$", 'is_portfolio=1&is_page=1&showposts=1&post_type=jh-portfolio&paged=$matches[2]', $portfolio_file );
+
+//portfolio single
+if( file_exists( $single_file = get_template_directory() . '/' . get_option( 'jhp_template_single', 'portfolio-single.php' ) ) );
+else $single_file = dirname( __FILE__ ) . '/template/portfolio.single.php';
+$single_base = untrailingslashit(get_option('jhp_single_base', '%category%'));
+$single_base = trim( str_ireplace( '%category%', '[^\/]*', $single_base ), '/' );
+
+tj_add_rewrite_rule( "^$portfolio_base" . ( $single_base ? "/$single_base" : "" ) . "/([^\/]*)/?$", 'is_portfolio_single=1&name=$matches[1]&post_type=jh-portfolio', $single_file );
+
+tj_add_rewrite_rule( "^$portfolio_base/([^/]*)/?", 'is_portfolio=1&taxonomy=jh-portfolio-category&term=$matches[1]&post_type=jh-portfolio', $portfolio_file );
+
 ?>
