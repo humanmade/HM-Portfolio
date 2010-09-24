dc25c18e9b2df69bc4617164c7cafccbd058b470
Joe Hoyle <joehoyle@gmail.com>
2010-09-24 13:38:43 +0100

    Renamed rewrites file

--- jhp.rewrite.php
+++ jhp.rewrite.php
@@ -4,7 +4,6 @@ $portfolio_base = get_option('jhp_url_base', 'portfolio');
 //portfolio home
 if( file_exists( $portfolio_file = get_template_directory() . '/' . get_option( 'jhp_template_home', 'portfolio-home.php' ) ) );
 else $portfolio_file = dirname( __FILE__ ) . '/template/portfolio.php';
-tj_add_rewrite_rule( "^$portfolio_base(/page/([0-9]*))?/?$", 'is_portfolio=1&is_page=1&showposts=1&post_type=jh-portfolio&paged=$matches[2]', $portfolio_file );
 
 //portfolio single
 if( file_exists( $single_file = get_template_directory() . '/' . get_option( 'jhp_template_single', 'portfolio-single.php' ) ) );
@@ -20,18 +19,17 @@ else $category_file = dirname( __FILE__ ) . '/template/portfolio.php';
 if( file_exists( $tag_file = get_template_directory() . '/' . get_option( 'jhp_template_tag', 'portfolio-tag.php' ) ) );
 else $tag_file = dirname( __FILE__ ) . '/template/portfolio.php';
 
+//portfolio home
+tj_add_rewrite_rule( "^$portfolio_base(/page/([0-9]*))?/?$", 'is_portfolio=1&static=1&is_page=1&post_type=jh-portfolio&paged=$matches[2]', $portfolio_file );
+
 //category
 tj_add_rewrite_rule( "^$portfolio_base/([^/]*)(/page/([0-9]*))?/?$", 'is_portfolio=1&is_portfolio_category=1&taxonomy=jh-portfolio-category&term=$matches[1]&post_type=jh-portfolio&paged=$matches[3]', $category_file );
-
 
 //tag
 tj_add_rewrite_rule( "^$portfolio_base/tag/([^/]*)(/page/([0-9]*))?/?$", 'is_portfolio=1&is_portfolio_tag=1&taxonomy=jh-portfolio-tag&term=$matches[1]&post_type=jh-portfolio&paged=$matches[3]', $tag_file );
 
-
 //single
 tj_add_rewrite_rule( "^$portfolio_base" . ( $single_base ? "/$single_base" : "" ) . "/([^\/]*)/?$", 'is_portfolio_single=1&name=$matches[1]&post_type=jh-portfolio', $single_file );
-
-
 
 
 function jhp_get_single_permastruct() {
