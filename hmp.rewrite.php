457943c8be4487a43440ace69ba6259a6a2f7953
Matthew Haines-Young <matthew@humanmade.co.uk>
2011-05-13 17:06:43 +0100

    - update namespacing

--- hmp.rewrite.php
+++ hmp.rewrite.php
@@ -2,4 +2,4 @@
 
 $template = get_template_directory() . '/archive-hmp-entry.php';
 $portfolio_base = get_option('hmp_url_base', 'portfolio');
-tj_add_rewrite_rule( "^$portfolio_base(/page/([0-9]*))?/?$", 'post_type=hmp-entry&paged=$matches[2]' );
+hm_add_rewrite_rule( "^$portfolio_base(/page/([0-9]*))?/?$", 'post_type=hmp-entry&paged=$matches[2]' );
