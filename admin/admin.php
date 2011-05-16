597e0b6b5bab6b2684c1b6e1916a868530f09f03
Matthew Haines-Young <matthew@humanmade.co.uk>
2011-05-16 09:32:09 +0100

    - fix path to admin icons and move code to admin.php - fix some conflicts with HM Core

--- admin/admin.php
+++ admin/admin.php
@@ -1,6 +1,26 @@
 <?php
 include_once('meta-boxes.php');
 
+// Admin Icons
+add_action( 'admin_head', 'hmp_icons' );
+function hmp_icons() {
+	$url = WP_PLUGIN_URL . '/' . 'HM-Portfolio/' ;
+    ?>
+    <style type="text/css" media="screen">
+        #menu-posts-hmp-entry .wp-menu-image {
+            background: url(<?php echo $url; ?>admin/icon.png) no-repeat 0 top !important;
+        }
+		#menu-posts-hmp-entry:hover .wp-menu-image, #menu-posts-hmp-entry.wp-has-current-submenu .wp-menu-image {
+            background-position:0 bottom !important;
+        }
+        #icon_hm-portfolio { background: url(<?php echo $url; ?>admin/icon_large.png) no-repeat; }
+        <?php if (($_GET['post_type'] == 'hmp-entry') || ($post_type == 'hmp-entry')) : ?>
+			#icon-edit { background: url(<?php echo $url; ?>admin/icon_large.png) no-repeat; }		
+			.icon32 { margin: 11px 6px 3px 0; }
+		<?php endif; ?>
+    </style><?php
+}
+
 add_action( 'admin_menu', 'hmp_add_meta_boxes' );
 function hmp_add_meta_boxes() {
 	
@@ -81,7 +101,7 @@ function hmp_options_page() {
 	?>
 	
 	<div class="wrap">
-		<div id="icon-options-hmp-entry" class="icon32"></div><h2>Portfolio Settings</h2>
+		<?php screen_icon( 'icon_hm-portfolio' ); ?><h2>Portfolio Settings</h2>
 	
 		<?php hmp_upgrade_jhp_notice() ?>		
 		
