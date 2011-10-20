4928eb8995b2982e01d31a51978438bacac47daa
richard swain <richard@humanmade.co.uk>
2011-10-20 18:26:37 +0100

    fixed updates to hmcore

--- plugin.php
+++ plugin.php
@@ -45,7 +45,7 @@ function hmp_check_plugin_compatibility() {
 //HM Core setup
 define( 'HM_ENABLE_ACCOUNTS', false );
 if( !function_exists( 'hm' ) )
-	include_once('HM Core/hm_core.php');
+	include_once('HM Core/hm-core.plugin.php');
 
 //Template rewrite
 include_once('hmp.functions.php');
