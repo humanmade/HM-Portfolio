c198bd31f3061cdf5431bccda9781040f165117f
Tom Willmot <tom@humanmade.co.uk>
2012-10-11 16:35:51 +0100

    Whitespace

--- plugin.php
+++ plugin.php
@@ -29,17 +29,17 @@ function hmp_check_plugin_compatibility() {
 	// check compatibility
 	global $wp_version;
 	$php_version = phpversion();
-	
-	include_once( ABSPATH . 'wp-admin/includes/plugin.php' );  
-	
+
+	include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
 	if( version_compare( $wp_version, '3.0', '<') ) {
-		deactivate_plugins(HMPPATH . '/plugin.php'); 
+		deactivate_plugins(HMPPATH . '/plugin.php');
 		die('HM Portfolio 0.9.7+ requires WordPress 3.0+, please download HM Portfolio 0.9.6 <a href="http://downloads.wordpress.org/plugin/hmp-portfolio.0.9.6.zip">here</a> for older versions of WordPress. ');
   	} elseif( version_compare( $php_version, '5', '<') ) {
-  		deactivate_plugins(HMPPATH . '/plugin.php'); 
+  		deactivate_plugins(HMPPATH . '/plugin.php');
 		die('HM Portfolio requires PHP 5+');
   	}
-  	
+
 }
 
 //HM Core setup
@@ -66,12 +66,12 @@ include_once('hmp.functions.deprecated.php');
 add_action( 'init', 'hmp_register_post_types' );
 function hmp_register_post_types() {
 	//register extra taxonomy
-	register_taxonomy( 'hmp-entry-category', 'hmp-entry', array( 
+	register_taxonomy( 'hmp-entry-category', 'hmp-entry', array(
 		'hierarchical' => true,
 		'show-ui' => true,
 		'rewrite' => array( 'slug' =>  get_option('hmp_url_base', 'portfolio') ),
 		'query_var' => 'term',
-		'labels' => array( 
+		'labels' => array(
 			'name' => 'Portfolio Categories',
 			'singular_name' => 'Portfolio Category',
 			'search_items' => 'Search Portfolio Categories',
@@ -80,14 +80,14 @@ function hmp_register_post_types() {
 			'add_new_item' => 'Add Portfolio Category',
 			'new_item_name' => 'New Portfolio Category Name',
 			'view' => 'View Entry' ),
-		));	
-	register_post_type( 'hmp-entry', array( 
+		));
+	register_post_type( 'hmp-entry', array(
 			'singular_label' => 'Report',
 			'public' => true,
 			'inherit_type' => 'post',
 			'taxonomies' => array( 'hmp-entry-category', 'post_tag' ),
 			'supports' => array( 'editor', 'title', 'revisions', 'custom-fields', 'thumbnail' ),
-			'labels' => array( 
+			'labels' => array(
 				'name' => 'Portfolio',
 				'singular_name' => 'Entry',
 				'not_found' => 'No entries found',
@@ -95,17 +95,17 @@ function hmp_register_post_types() {
 				'search_items' => 'Search Entries',
 				'edit_item' => 'Edit Entry',
 				'add_new_item' => 'Add New Entry' ),
-			'has_archive' => true	
 			'rewrite' => array( 'slug' => apply_filters( 'hm-portfolio-entry-rewrite-rule', get_option( 'hmp_url_base', 'portfolio' ) . '/%hmp-entry-category%' ) ),
+			'has_archive' => true
 		)
 	);
 }
 
 function hmp_remove_featured() {
 	if( get_option( 'hmp_manage_featured', true ) && get_option( 'hmp_post_type' ) ) {
-		foreach( get_option( 'hmp_post_type' ) as $post_type ) {	
+		foreach( get_option( 'hmp_post_type' ) as $post_type ) {
 			remove_meta_box( 'postimagediv', $post_type, 'side' );
-		} 	
+		}
 	}
 }
 add_action( 'do_meta_boxes', 'hmp_remove_featured' );
